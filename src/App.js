8d2edd9025c786f9cc5c670aa7245b0cbcc9d2cb
chengdongzhang0710 <chengdongzhang0710@outlook.com>
2023-06-25 16:48:25 +0800

    add lazy loading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
+import { lazy, Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import Layout from "@/pages/Layout";
-import Login from "@/pages/Login";
-import AuthRoute from "@/components/AuthRoute";
-import Home from "@/pages/Home";
-import Article from "@/pages/Article";
-import Publish from "@/pages/Publish";
 import { history, HistoryRouter } from "@/utils/history";
+import AuthRoute from "@/components/AuthRoute";
+import { Spin } from "antd";
+
+const Login = lazy(() => import("@/pages/Login"));
+const Layout = lazy(() => import("@/pages/Layout"));
+const Home = lazy(() => import("@/pages/Home"));
+const Article = lazy(() => import("@/pages/Article"));
+const Publish = lazy(() => import("@/pages/Publish"));
 
 function App() {
   return (
     <div className="App">
-      <HistoryRouter history={history}>
-        <Routes>
-          <Route path="/" element={ <AuthRoute><Layout/></AuthRoute> }>
-            <Route index element={ <Home/> }/>
-            <Route path="article" element={ <Article/> }/>
-            <Route path="publish" element={ <Publish/> }/>
-          </Route>
-          <Route path="/login" element={ <Login/> }/>
-        </Routes>
+      <HistoryRouter history={ history }>
+        <Suspense
+          fallback={ (
+            <div style={ { textAlign: "center", marginTop: 400 } }>
+              <Spin size="large"/>
+            </div>) }
+        >
+          <Routes>
+            <Route path="/" element={ <AuthRoute><Layout/></AuthRoute> }>
+              <Route index element={ <Home/> }/>
+              <Route path="article" element={ <Article/> }/>
+              <Route path="publish" element={ <Publish/> }/>
+            </Route>
+            <Route path="/login" element={ <Login/> }/>
+          </Routes>
+        </Suspense>
       </HistoryRouter>
     </div>
   );
